bcfc12e657a0594b0a8f18cc8d3ab5fd62372818
Scriptor25 <f.schreiber.2006@gmail.com>
2026-02-24 12:57:57 +0100

    added minification to build process

--- scripts/process.js
+++ scripts/process.js
@@ -1,14 +1,30 @@
+import CleanCSS from "clean-css";
 import matter from "gray-matter";
 import hljs from "highlight.js";
 import { parse, Renderer } from "marked";
 import fs from "node:fs";
 import path from "node:path";
+import { minify as terserMinifyJS } from "terser";
+import { minify as terserMinifyHTML } from "html-minifier-terser";
 import ts from "typescript";
 import yaml from "yaml";
 
 /**
  * @typedef {{ srcDir: string, dstDir: string, debug: boolean }} Config
  */
+
+/**
+ * @param {string} path
+ * @returns {string}
+ */
+const readFile = (path) => fs.readFileSync(path, "utf-8");
+
+/**
+ * @param {string} path
+ * @param {string} data
+ * @returns {void}
+ */
+const writeFile = (path, data) => fs.writeFileSync(path, data, "utf-8");
 
 /**
  * @param {boolean} debug
@@ -33,8 +49,8 @@ function createDefineDebugTransformer(debug) {
  * @param {Record<string, unknown>} data
  * @param {string} content
  */
-function instantiate(templatePath, global, data, content) {
-    let template = fs.readFileSync(templatePath, "utf-8");
+function instantiateTemplate(templatePath, global, data, content) {
+    let template = readFile(templatePath);
 
     template = template.replaceAll("%content%", content);
 
@@ -52,13 +68,186 @@ function instantiate(templatePath, global, data, content) {
 /**
  * @param {Config} config
  * @param {string} srcPath
+ * @param {string} dstPath
  */
-export function processFile(config, srcPath) {
+async function processJS(config, srcPath, dstPath) {
+    const input = readFile(srcPath);
+
+    try {
+        const result = await terserMinifyJS(input, {
+            compress: true,
+        });
+
+        writeFile(dstPath, result.code);
+    } catch (error) {
+        console.error("in file %s: %s", srcPath, error);
+    }
+}
+
+/**
+ * @param {Config} config
+ * @param {string} srcPath
+ * @param {string} dstPath
+ */
+async function processCSS(config, srcPath, dstPath) {
+    const input = readFile(srcPath);
+    const result = new CleanCSS().minify(input);
+
+    for (const message of result.errors) {
+        console.error("in file %s: %s", srcPath, message);
+    }
+
+    for (const message of result.warnings) {
+        console.warn("in file %s: %s", srcPath, message);
+    }
+
+    writeFile(dstPath, result.styles);
+}
+
+/**
+ * @param {Config} config
+ * @param {string} srcPath
+ * @param {string} dstPath
+ */
+async function processHTML(config, srcPath, dstPath) {
+    const input = readFile(srcPath);
+
+    try {
+        const result = await terserMinifyHTML(input, {
+            collapseWhitespace: true,
+            removeComments: true,
+        });
+
+        writeFile(dstPath, result);
+    } catch (error) {
+        console.error("in file %s: %s", srcPath, error);
+    }
+}
+
+/**
+ * @param {Config} config
+ * @param {string} srcPath
+ * @param {string} dstPath
+ */
+async function processTS(config, srcPath, dstPath) {
+    const input = readFile(srcPath);
+    const result = ts.transpileModule(input, {
+        compilerOptions: {
+            target: ts.ScriptTarget.ES2020,
+            module: ts.ModuleKind.ES2020,
+            moduleDetection: ts.ModuleDetectionKind.Force,
+            moduleResolution: ts.ModuleResolutionKind.Bundler,
+            esModuleInterop: false,
+            isolatedModules: true,
+            sourceMap: config.debug ?? false,
+            strict: true,
+        },
+        transformers: {
+            before: [createDefineDebugTransformer(config.debug)],
+        },
+        fileName: srcPath,
+    });
+
+    for (const diagnostic of result.diagnostics) {
+        switch (diagnostic.category) {
+            case ts.DiagnosticCategory.Message:
+            case ts.DiagnosticCategory.Suggestion:
+                console.log("in file %s: %s", diagnostic.messageText);
+                break;
+            case ts.DiagnosticCategory.Warning:
+                console.warn("in file %s: %s", diagnostic.messageText);
+                break;
+            case ts.DiagnosticCategory.Error:
+                console.error("in file %s: %s", diagnostic.messageText);
+                break;
+        }
+    }
+
+    const output = result.outputText;
+    const outputPath = dstPath.replace(/\.ts$/, ".js");
+
+    try {
+        const result = await terserMinifyJS(output, {
+            compress: true,
+        });
+
+        writeFile(outputPath, result.code);
+    } catch (error) {
+        console.error("in file %s: %s", srcPath, error);
+
+        writeFile(outputPath, output);
+    }
+
+    if (config.debug) {
+        writeFile(`${outputPath}.map`, result.sourceMapText);
+        fs.copyFileSync(srcPath, dstPath);
+    }
+}
+
+/**
+ * @param {Config} config
+ * @param {string} srcPath
+ * @param {string} dstPath
+ */
+async function processMD(config, srcPath, dstPath) {
+    const input = readFile(srcPath);
+
+    const { data, content } = matter(input);
+
+    const renderer = new Renderer();
+
+    renderer.code = ({ text, lang }) => {
+        const valid = lang && hljs.getLanguage(lang);
+        const code = valid ? hljs.highlight(text, { language: lang }).value : hljs.highlightAuto(text).value;
+
+        return `<pre><code class="hljs ${lang ?? ""}">${code}</code></pre>`;
+    };
+    renderer.code = renderer.code.bind(renderer);
+
+    const html = parse(content, { async: false, renderer });
+
+    const template = data.template;
+
+    const templatePath = path.join(config.srcDir, "templates", `${template}.html`);
+
     const global = (() => {
-        const input = fs.readFileSync(path.join(config.srcDir, "config.yaml"), "utf-8");
+        const input = readFile(path.join(config.srcDir, "config.yaml"));
         return yaml.parse(input);
     })();
 
+    const output = instantiateTemplate(templatePath, global, data, html);
+    const outputPath = dstPath.replace(/\.md$/, ".html");
+
+    try {
+        const result = await terserMinifyHTML(output, {
+            collapseWhitespace: true,
+            removeComments: true,
+        });
+
+        writeFile(outputPath, result);
+    } catch (error) {
+        console.error("in file %s: %s", srcPath, error);
+
+        writeFile(outputPath, output);
+    }
+}
+
+/**
+ * @type {Record<string, (config: Config, srcPath: string, dstPath: string) => (void | Promise<void>)>}
+ */
+const process = {
+    ".js": processJS,
+    ".css": processCSS,
+    ".html": processHTML,
+    ".ts": processTS,
+    ".md": processMD,
+};
+
+/**
+ * @param {Config} config
+ * @param {string} srcPath
+ */
+export function processFile(config, srcPath) {
     const relPath = path.relative(config.srcDir, srcPath);
     const dstPath = path.join(config.dstDir, relPath);
 
@@ -74,66 +263,11 @@ export function processFile(config, srcPath) {
         throw new Error("Path must either point to a file or directory.");
     }
 
-    switch (path.extname(srcPath)) {
-        case ".ts": {
-            const input = fs.readFileSync(srcPath, "utf-8");
-            const result = ts.transpileModule(input, {
-                compilerOptions: {
-                    target: ts.ScriptTarget.ES2020,
-                    module: ts.ModuleKind.ES2020,
-                    moduleDetection: ts.ModuleDetectionKind.Force,
-                    moduleResolution: ts.ModuleResolutionKind.Bundler,
-                    esModuleInterop: false,
-                    isolatedModules: true,
-                    sourceMap: config.debug ?? false,
-                    strict: true,
-                },
-                transformers: {
-                    before: [createDefineDebugTransformer(config.debug)],
-                },
-                fileName: srcPath,
-            });
-
-            const outputPath = dstPath.replace(/\.ts$/, ".js");
-            fs.writeFileSync(outputPath, result.outputText, "utf-8");
-
-            if (config.debug) {
-                fs.writeFileSync(`${outputPath}.map`, result.sourceMapText, "utf-8");
-                fs.copyFileSync(srcPath, dstPath);
-            }
-            break;
-        }
-
-        case ".md": {
-            const input = fs.readFileSync(srcPath, "utf-8");
-
-            const { data, content } = matter(input);
-
-            const renderer = new Renderer();
-
-            renderer.code = ({ text, lang }) => {
-                const valid = lang && hljs.getLanguage(lang);
-                const code = valid ? hljs.highlight(text, { language: lang }).value : hljs.highlightAuto(text).value;
-
-                return `<pre><code class="hljs ${lang ?? ""}">${code}</code></pre>`;
-            };
-            renderer.code = renderer.code.bind(renderer);
-
-            const html = parse(content, { async: false, renderer });
-
-            const template = data.template;
-
-            const templatePath = path.join(config.srcDir, "templates", `${template}.html`);
-            const output = instantiate(templatePath, global, data, html);
-
-            const outputPath = dstPath.replace(/\.md$/, ".html");
-            fs.writeFileSync(outputPath, output, "utf-8");
-            break;
-        }
-
-        default:
-            fs.copyFileSync(srcPath, dstPath);
-            break;
+    const extname = path.extname(srcPath);
+    if (extname in process) {
+        process[extname](config, srcPath, dstPath);
+    } else {
+        fs.copyFileSync(srcPath, dstPath);
     }
 }
 
